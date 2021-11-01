178b52f85e4ac516f856797f9c0d21d2899f009d
Alexandra Marks <amarks93@gmail.com>
2021-11-01 14:05:10 -0400

    setup: week 0

---
+++ 001-two-number-sum.js
@@ -0,0 +1,18 @@
+// PROMPT
+// Given an array of numbers and a target number (a "sum"),
+// determine if any 2 numbers in the array add up to the sum.
+// Return true if any 2 different numbers within the array add up to sum.
+// Return false if no 2 numbers in the array add up to sum.
+
+// RESTATE
+//
+
+// EXAMPLES
+
+// APPROACH
+
+// CODE
+
+// TEST
+
+// OPTIMIZE
---
+++ 002-multiplicative-persistence.js
@@ -0,0 +1,17 @@
+// PROMPT
+// Given a non-negative integer, write a function that returns its multiplicative persistence--
+// the number of times you must multiply the digits in a number together until you reach
+// a single digit product.
+
+// RESTATE
+//
+
+// EXAMPLES
+
+// APPROACH
+
+// CODE
+
+// TEST
+
+// OPTIMIZE
