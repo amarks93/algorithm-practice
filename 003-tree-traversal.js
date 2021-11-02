2878b7ad39ea2bba022660c1cfdffd230b33d2f4
Alexandra Marks <amarks93@gmail.com>
2021-11-02 12:05:21 -0400

    WIP: 003: BFS need to optimize

--- 003-tree-traversal.js
+++ 003-tree-traversal.js
@@ -34,14 +34,43 @@ h.children.push(m);
 d.children.push(i, j);
 
 // RESTATE
-//
+// Given the head node of a tree (not a bst) and a callback function, iterate through each
+// node in the tree using the callback function.
+// Iterate three separate ways: BFS, DFS(pre-order), DFS(post-order)
 
 // EXAMPLES
+// BFS -> a b c d e f g h i j k l m
+// DFS PRE -> a b e k l c f g h m d i j
+// DFS POST -> k l e b f g m h c i j d a
 
 // APPROACH
+// BFS
+//   Create a queue, visit a node, process it's value with the callback function
+//   If node in the queue has children, push children into the queue
+//   After processing with the callback and checking for children, get rid of the node
+// DFS PRE
+// DFS POST
 
 // CODE
+// BFS
+function visitNode(node) {
+  // log the function? store in an array?
+  console.log(node);
+}
+
+function bfsTraversal(node) {
+  let queue = [node];
+  while (queue.length >= 1) {
+    let firstNode = queue[0];
+    visitNode(firstNode.value);
+    if (firstNode.children) {
+      for (let child of firstNode.children) queue.push(child);
+    }
+    queue.shift();
+  }
+}
 
 // TEST
+console.log("alphabetical", bfsTraversal(a));
 
 // OPTIMIZE
