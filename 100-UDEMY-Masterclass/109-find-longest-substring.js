c707b3d451033c2c22c9ef69c1ae8630bc705341
Alexandra Marks <amarks93@gmail.com>
2021-11-10 11:05:56 -0500

    complete: 203, 109, 006.1, 110

--- 100-UDEMY-Masterclass/109-find-longest-substring.js
+++ 100-UDEMY-Masterclass/109-find-longest-substring.js
@@ -3,15 +3,55 @@
 // of the longest substring with all distinct characters.
 
 // RESTATE
+// Given a string, need to find the longest strand of unique characters
 
 // EXAMPLES
+// '' -> 0
+// 'rithmschool' -> 7
+// 'aaaaa' -> 1
+// 'banana' -> 3
 
 // APPROACH
+// create a memo
+// set up a variable for the longest length
+// set up start pointers
+// loop through and mark down the start and end points (end is i in the loop),
+// for each index in the string, add the character to the memo with key: char and value: index
+// keep looping and if you find a character that's already in the memo
+// change the starting point to the instance of the character
+// start moving the end point again
+// lather rinse repeat
 
 // CODE
+function findLongestSubstring(str) {
+  let longestSubstring = 0;
+  let memo = {};
+  let start = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    let end = i;
+    let curChar = str[i];
+
+    if (memo[curChar]) {
+      start = Math.max(start, memo[curChar]);
+    } else {
+      longestSubstring = Math.max(longestSubstring, end - start + 1);
+      memo[curChar] = end + 1;
+    }
+  }
+
+  return longestSubstring;
+}
 
 // TEST
+console.log("0", findLongestSubstring(""));
+console.log("7", findLongestSubstring("rithmschool"));
+console.log("1", findLongestSubstring("aaaaa"));
+console.log("3", findLongestSubstring("banana"));
 
 // OPTIMIZE
-// time -
-// space -
+// time - O (n)
+//  loop -> n
+//  access object -> 1
+// space - O (n)
+//  created an object -> n
---
+++ 100-UDEMY-Masterclass/110-power.js
@@ -0,0 +1,34 @@
+// PROMPT
+// Write a function called power which accepts a base and an exponent. The function should
+// return the power of the base to the exponent. This function should mimic the
+// functionality of Math.pow() - do not worry about negative bases and exponents.
+
+// RESTATE
+// Given a base and an exponent, return the power
+
+// EXAMPLE
+// (2,0) => 1
+// (2,2) => 4
+// (2,4) => 16
+
+// APPROACH
+// Use recursion
+// Set up a base case where if the second argument is 0, return 1
+// Set up a recursive case where you multiply the base by the return of
+// the function being called on the base and the exponent - 1
+// By the end the power will be the base multiplied the correct number of times
+
+// CODE
+function power(base, exponent) {
+  if (exponent === 0) return 1;
+  return base * power(base, exponent - 1);
+}
+
+// TEST
+console.log("1", power(2, 0));
+console.log("4", power(2, 2));
+console.log("16", power(2, 4));
+
+// OPTIMIZE
+// time -
+// space -
